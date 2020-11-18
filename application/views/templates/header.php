92e624001fead778d7c2597b44ae9590d7e721b1
Krigovszki Bálint <krigovszki.balint@gmail.com>
2020-11-18 04:41:40 +0100

    Reading posts from DB

---
+++ application/views/templates/header.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title>Webprog3</title>
+<link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css">
+</head>
+<body>
+<!-- 	<nav class="navbar navbar-inverse">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="<?php echo base_url(); ?>">Webprog3</a>
+			</div>
+			<div id="navbar">
+				<ul class="nav navbar-nav">
+					<li><a href="<?php echo base_url(); ?>">Home</a></li>
+					<li><a href="<?php echo base_url(); ?>/about">About</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav> -->
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo base_url(); ?>">Home
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo base_url(); ?>posts">Blog</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container-fluid">
